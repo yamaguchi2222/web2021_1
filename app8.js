7fc438167a278f78ab4e3293a50c1b1fc81ae383
yamaguchi2222 <s2032143us@s.chibakoudai.js>
2024-06-17 12:49:03 +0900

    ‘app8.jsの追加’

--- app8.js
+++ app8.js
@@ -14,22 +14,21 @@ app.get("/", (req, res) => {
 
 app.get("/car", (req, res) => {
     //console.log(req.query.pop);    // ①
-    let desc = "";
-    if( req.query.desc ) desc = " desc";
+   
     let sql = `
     select car.id, car.name, maker.name as name2
     from car
     inner join maker
-    on cre.maker_id=maker.id
+    on car.maker_id=maker.id
     `
     //console.log(sql);    // ②
     db.serialize( () => {
-        db.all(sql, (error, data) => {
+        db.all(sql, (error, row) => {
             if( error ) {
                 res.render('show', {mes:"エラーです"});
             }
-            //console.log(data);    // ③
-            res.render('car', {data:data});
+            console.log(row);    // ③
+            res.render('car', {row:row});
         })
     })
 })
